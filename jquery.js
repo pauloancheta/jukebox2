f706a1c62bbcf0a8907e130374bef13890375bd1
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-01-26 16:10:10 -0800

    deleted unnecessary file

--- jquery.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $('button').on('click' ,function(){
-    $(this).html('Playing...');
-    var songString = prompt("Welcome to JukeBox!\nEnter a song to play:");
-    var song = parseSong(songString);
-    playSong(song, 20);
-    $(this).html('Play');
-  });
-});
